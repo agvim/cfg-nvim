8ff56c245a3242d9619288357efa40bc6a60a6d7
agvim <agvim@users.noreply.github.com>
2020-01-28 10:04:53 +0100

    allop piping in gnvim, remove unused script

--- gnvim.sh
+++ gnvim.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# lauch kitty with no shortcuts and execute neovim
-kitty -c ~/.config/kitty/kittygnvim.conf nvim $@ &
-KITTYPID=$!
+# check stdin or pipe
+if [ -t 0 ]; then
+    # lauch kitty with no shortcuts and execute neovim
+    kitty -c ~/.config/kitty/kittygnvim.conf nvim $@ &
+    KITTYPID=$!
+else
+    # export PIPED="$(cat <&0)" # note that this has a kernel limitation as PIPED is expanded
+    WRAPPERPID=$$
+    # run with piped input
+    kitty -c ~/.config/kitty/kittygnvim.conf /bin/sh -c "cat /proc/$WRAPPERPID/fd/0 | nvim $@" &
+    KITTYPID=$!
+fi
 
 # wait for kitty to spawn nvim and get its PID
 TARGETPID=""
