6fddf0a7c74ca263037c8289d9dff822662a8aff
agvim <agvim@users.noreply.github.com>
2021-11-02 17:42:21 +0100

    added missing onupdate file

---
+++ onupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# we need the init.vim files to be linked in order to avoid installing
+# unnecessary stuff and installing custom stuff
+
+# if the homeshick repo has an update, re-link cfg-nvim files before updating
+# nvim
+if [[ $1 == 1 ]]; then
+    homeshick=$HOME/.homesick/repos/homeshick/bin/homeshick
+    $homeshick link cfg-nvim
+fi
+
+nvim "+PlugUpgrade" "+PlugUpdate" '+qa!'
